9d746cd0ccd8e6130eaf0204f7f34f8577eb662c
Phinjock Sherpa <phinjock@gmail.com>
2022-09-21 13:45:10 -0700

    Created controller & routes, completed user routes WIP rest of routes

---
+++ controller/dashboard-routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const { Post, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  console.log(req.session);
+  try {
+    const postData = await Post.findAll({
+      where: { user_id: req.session.user_id },
+      include: [User],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+
+    res.render("need to change accroding to handlebar--PJ", {
+      posts,
+      layout: "need to change accroding to handlebar--PJ",
+      loggedIn: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
