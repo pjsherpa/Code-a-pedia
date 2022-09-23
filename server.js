5d212e8c2bff87058382fc28666cc7b172506373
Phinjock Sherpa <phinjock@gmail.com>
2022-09-22 20:37:40 -0700

    sql linked and server working

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const routes = require("./controllers");
+const routes = require("./controller");
 const helpers = require("./utils/helpers");
 
 const sequelize = require("./config/connection");
