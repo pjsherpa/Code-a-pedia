02ef9e20d3d7503759f57520055afa6905a97d1d
Phinjock Sherpa <phinjock@gmail.com>
2022-09-22 19:57:17 -0700

    linking seeds to model, routes to controllers checking codes to fix linkage WIP handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -1,60 +1,59 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
+  event.preventDefault();
+
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      localStorage.setItem("logged_in", "true");
+      document.location.replace("/profile");
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
       });
-  
-      if (response.ok) {
-        localStorage.setItem('loggedIn', 'true');
-        document.location.replace('/profile');
-      } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Something went wrong!'
-          })
-      }
     }
-  };
-  
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, username, password }),
-        headers: { 'Content-Type': 'application/json' },
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      localStorage.setItem("logged_in", "true");
+      document.location.replace("/profile");
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
       });
-  
-      if (response.ok) {
-        localStorage.setItem('loggedIn', 'true');
-        document.location.replace('/profile');
-      } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Something went wrong!'
-          })
-      }
     }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
-  
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
--- seeds/seesPosts.js
+++ seeds/seesPosts.js
@@ -1,22 +1,22 @@
-const { Post } = require('../models');
+const { Post } = require("../models");
 
 const postData = [
-    {
-        title:"Word1",
-        content:"Great!",
-        user_id: 1
-    },
-    {
-        title:"Word2",
-        content:"Awsome",
-        user_id:2
-    },
-    {
-        title:"Word3",
-        content:"Love it!",
-        user_id:3
-    }
-]
+  {
+    title: "Word1",
+    body: "Great!",
+    user_id: 1,
+  },
+  {
+    title: "Word2",
+    body: "Awsome",
+    user_id: 2,
+  },
+  {
+    title: "Word3",
+    body: "Love it!",
+    user_id: 3,
+  },
+];
 
 const seedPosts = () => Post.bulkCreate(postData);
 
