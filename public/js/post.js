39d9ea8b3748c0f9bec8acc99e399c9808d3d9ea
Savannah Fausto <savannahfausto@Savannahs-MBP.attlocal.net>
2022-09-22 20:26:50 -0700

    adds post.js

---
+++ public/js/post.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+    //check PJ: link #post
+    const name = document.querySelector('#post').value.trim();
+    const description = document.querySelector('#post').value.trim();
+  
+    if (name && description) {
+      const response = await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/post');
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/post/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/post');
+      } else {
+        //when change to sweetalert, change project to post
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-project-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.project-list')
+    .addEventListener('click', delButtonHandler);
+  
