b972b69de4ef3c90bd28f4d6f51587b77fbd9c84
kaihuan Huang <huanhuan@kaihuans-MBP.attlocal.net>
2022-09-24 20:10:36 -0700

    update login and signup

--- seeds/seedUsers.js
+++ seeds/seedUsers.js
@@ -3,14 +3,17 @@ const { User } = require("../models");
 const userData = [
   {
     username: "PJ",
+    useremail: "pj@gmail.com",
     password: "1234",
   },
   {
     username: "Kai",
+    useremail: "kai@gmail.com",
     password: "1234",
   },
   {
     username: "Sav",
+    useremail:"sav@gmail.com",
     password: "1234",
   },
 ];
