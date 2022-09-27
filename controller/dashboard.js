77f164777e377051856da36f069c72ccc08b034e
Phinjock Sherpa <phinjock@gmail.com>
2022-09-26 20:57:55 -0700

    refactored codes and sweet alert now working for signup

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -2,29 +2,6 @@ const router = require("express").Router();
 const { Post, User } = require("../models");
 const withAuth = require("../utils/auth");
 
-// localhost:3001/dashboard/
-// router.get("/codeword", async (req, res) => {
-//   try {
-//      // Get all data
-//      const postData = await Post.findAll({
-      
-//       include: [User],
-//     });
-//     const posts = postData.map((post) => post.get({ plain: true }));
-
-//     // Pass serialized data and session flag into template
-//       res.render("codeword", {
-//       posts,
-//       layouts: "dashboard",
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
 router.get("/", withAuth, async (req, res) => {
   console.log(req.session);
   try {
